9b2e29cc81d6fc7ee03defdde885f43507ab8210
Seshu K <seshubabu.kalakurthi@gmail.com>
2025-09-30 19:41:18 +0100

    added unit test cases

--- BMILabtestproject/Test1.cs
+++ BMILabtestproject/Test1.cs
@@ -1,4 +1,7 @@
-﻿namespace BMILabtestproject
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BMICalculator;
+
+namespace BMILabtestproject
 {
     [TestClass]
     public sealed class Test1
@@ -6,6 +9,18 @@
         [TestMethod]
         public void TestMethod1()
         {
+            BMI bmi = new BMI() { WeightStones = 12, WeightPounds = 0, HeightFeet = 5, HeightInches = 10 };
+            Assert.AreEqual(bmi.BMICategory, BMICategory.Normal);
+        }
+
+        [DataTestMethod]
+        [DataRow(12, 0, 5, 10, BMICategory.Normal)]
+        [DataRow(15, 0, 5, 10, BMICategory.Obese)]
+
+        public void TestMethod2(int ws, int wp, int hf, int hi, BMICategory cat)
+        {
+            BMI bmi = new BMI() { WeightStones = ws, WeightPounds = wp, HeightFeet = hf, HeightInches = hi };
+            Assert.AreEqual(bmi.BMICategory, cat);
         }
     }
 }
---
+++ test2/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace test2
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
